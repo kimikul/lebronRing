eedbc35424a83f331e270a59e1b4ba78e567d6ed
lambtron <andyjiang@gmail.com>
2012-06-25 23:13:09 -0400

    made it a little harder haha

--- public/game.js
+++ public/game.js
@@ -112,16 +112,16 @@ function startGame() {
 		// ring will move away from the player
 		if (nearCollide(player, ring)) {
 			if (keydown.left && ring.x > 5) {
-				ring.x -= 2;
+				ring.x -= 5;
 			}
 			if (keydown.right && ring.x < CANVAS_WIDTH - ring.width) {
-				ring.x += 2;
+				ring.x += 5;
 			}
 			if (keydown.down && ring.y < CANVAS_HEIGHT - ring.height) {
-				ring.y += 2;
+				ring.y += 5;
 			}
 			if (keydown.up && ring.y > 0) {
-				ring.y -= 2;
+				ring.y -= 5;
 			}
 		}
 		// ring teleportation code

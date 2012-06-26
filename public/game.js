80d803fab25e8d6f49b85c00a3fc31ab0b8a6320
lambtron <andyjiang@gmail.com>
2012-06-26 11:28:21 -0400

    added dan gilbert

--- public/game.js
+++ public/game.js
@@ -35,25 +35,16 @@ function startGame() {
 	}
 
 	function update() {
-		if (keydown.left && player.x > 5) {
-			player.x -= 5;
-		}
-		if (keydown.right && player.x < CANVAS_WIDTH - player.width) {
-			player.x += 5;
-		}
-		if (keydown.down && player.y < CANVAS_HEIGHT - player.height) {
-			player.y += 5;
-		}
-		if (keydown.up && player.y > 0) {
-			player.y -= 5;
-		}
+		movePlayer();
 		moveRing();
+		moveOpponent();
 		checkCollisions();
 	}
 
 	function draw() {
 		canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		player.draw();
+		opponent.draw();
 		ring.draw();
 	}
 
@@ -63,13 +54,24 @@ function startGame() {
 		width: 58,
 		height: 74,
 		frame: 0,
+		spins: 0, // 0: no spin, 1: spin
+		radians: 0,	// degree of spin
 		spriteSheet: new Image(),
 		draw: function() {
 			this.spriteSheet.src = 'images/lebron-sheet.png';
 			if(timeElapsed(startDate) > 14 && !collides(player, ring)) {
 				this.frame = 2;
 			}
-			canvas.drawImage(this.spriteSheet, this.frame * 100, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+			if(this.spins == 0) {
+				canvas.drawImage(this.spriteSheet, this.frame * 100, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+			} else { // spin! canvas is
+				this.radians += 0.4;
+				canvas.save();
+				canvas.translate(player.x + player.width / 2, player.y + player.height / 2);
+				canvas.rotate(this.radians);
+				canvas.drawImage(this.spriteSheet, this.frame * 100, 0, this.width, this.height, -this.width / 2, -this.height / 2, this.width, this.height);
+				canvas.restore();
+			}
 		}
 	};
 
@@ -83,6 +85,38 @@ function startGame() {
 		draw: function() {
 			this.spriteSheet.src = 'images/ring.png';
 			canvas.drawImage(this.spriteSheet, this.frame * 100, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+		}
+	}
+
+	var opponent = {
+		x: 200,
+		y: 250,
+		width: 58,
+		height: 74,
+		frame: 0,
+		directionRight: 0, // 0: left, 1: right
+		directionDown: 0,  // 0: up, 1: down
+		spriteSheet: new Image(),
+		draw: function() {
+			this.spriteSheet.src = 'images/dan-gilbert-sheet.png';
+			canvas.drawImage(this.spriteSheet, this.frame * 100, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+		}
+	}
+
+	function movePlayer() {
+		if (player.spins == 0) {	// player can only move if he isn't spinning
+			if (keydown.left && player.x > 5) {
+				player.x -= 5;
+			}
+			if (keydown.right && player.x < CANVAS_WIDTH - player.width) {
+				player.x += 5;
+			}
+			if (keydown.down && player.y < CANVAS_HEIGHT - player.height) {
+				player.y += 5;
+			}
+			if (keydown.up && player.y > 0) {
+				player.y -= 5;
+			}
 		}
 	}
 
@@ -111,32 +145,30 @@ function startGame() {
 		}
 		// ring will move away from the player
 		if (nearCollide(player, ring)) {
-			if (keydown.left && ring.x > 5) {
-				ring.x -= 5;
-			}
-			if (keydown.right && ring.x < CANVAS_WIDTH - ring.width) {
-				ring.x += 5;
-			}
-			if (keydown.down && ring.y < CANVAS_HEIGHT - ring.height) {
-				ring.y += 5;
-			}
-			if (keydown.up && ring.y > 0) {
-				ring.y -= 5;
-			}
+			if (keydown.left && ring.x > 5) { ring.x -= 5; }
+			if (keydown.right && ring.x < CANVAS_WIDTH - ring.width) { ring.x += 5;	}
+			if (keydown.down && ring.y < CANVAS_HEIGHT - ring.height) {	ring.y += 5; }
+			if (keydown.up && ring.y > 0) {	ring.y -= 5; }
 		}
 		// ring teleportation code
-		if (ring.x + ring.width > CANVAS_WIDTH) {
-			ring.x = 40;
-		}
-		if (ring.x < 0) {
-			ring.x = CANVAS_WIDTH - ring.width - 40;
-		}
-		if (ring.y + ring.height > CANVAS_HEIGHT) {
-			ring.y = 40;
-		}
-		if (ring.y < 0) {
-			ring.y = CANVAS_HEIGHT - ring.height - 40;
-		}
+		if (ring.x + ring.width > CANVAS_WIDTH) { ring.x = 40; }
+		if (ring.x < 0) { ring.x = CANVAS_WIDTH - ring.width - 40; }
+		if (ring.y + ring.height > CANVAS_HEIGHT) { ring.y = 40; }
+		if (ring.y < 0) { ring.y = CANVAS_HEIGHT - ring.height - 40; }
+	}
+
+	function moveOpponent() {
+		// if hit a wall, change direction
+		if (opponent.x + opponent.width > CANVAS_WIDTH) { opponent.directionRight = 0; }
+		if (opponent.x < 0) { opponent.directionRight = 1; }
+		if (opponent.y + opponent.height > CANVAS_HEIGHT) { opponent.directionDown = 0; }
+		if (opponent.y < 0) { opponent.directionDown = 1; }
+		// move diagonally
+		if (opponent.directionRight == 0) { opponent.x -= 3; }
+		if (opponent.directionRight == 1) { opponent.x += 3; }
+		if (opponent.directionDown == 0) { opponent.y -= 3; }
+		if (opponent.directionDown == 1) { opponent.y += 3; }
+		console.log(opponent.directionRight + " and " + opponent.directionDown);
 	}
 
 	// collision detection
@@ -158,6 +190,7 @@ function startGame() {
 		if (collides(player, ring)) {
 			// change lebron to smile
 			player.frame = 1;
+			opponent.frame = 1;
 
 			// play nooooo sound
 			noClip.play();
@@ -169,10 +202,12 @@ function startGame() {
 			// show play again button
 			var playAgainButton = "<button class='btn btn-warning btn-large' type='submit' onclick='startGame()'>Play Again</button>";
 			$('#game').append(playAgainButton);
+		}
+		if (nearCollide(player, opponent)) {
+			// lebron spins
+			player.spins = 1;
 		} else {
-			//player.sprite = Sprite("lebron");
-			// console.log("not colliding");
-			//alert("HI");
+			player.spins = 0;
 		}
 
 	}
